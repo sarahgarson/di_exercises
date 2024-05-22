53829d9d5be5478560d6780a00c697d5973f8d48
Sarah Garson <sarah.garson.santos@gmail.com>
2024-05-22 22:28:48 +0300

    daily-challenge-stars-done

---
+++ week3/Day6/Daily-Challenge-Stars/script.js
@@ -0,0 +1,28 @@
+// Instructions
+
+// 1.  Write a JavaScript program that recreates the pattern below.
+
+// 2. Do this challenge twice: first by using one loop, then by using two nested for loops (Nested means one inside the other - check out this tutorial of nested loops).
+
+// 3. Do this Daily Challenge by yourself, without looking at the answers on the internet.
+
+//first: using one loop
+
+let star = "";
+
+/*for (i = 0; i < 6; i++) {
+  star += "*";
+  console.log(star);
+}*/
+
+//second:using two loops
+
+for (i = 0; i < 1; i++) {
+  let star2 = "";
+  console.log(star2);
+
+  for (j = 0; j < 6; j++) {
+    star2 += "*";
+    console.log(star2);
+  }
+}
