07708753d92634ca0a88cd6c3b7053c3703a92d9
Sarah Garson <sarah.garson.santos@gmail.com>
2024-05-27 18:55:09 +0300

    daily-challenge-bonus-unfinished

---
+++ week4/Day4/Daily-Challenge-Planets/script.js
@@ -0,0 +1,57 @@
+//Instructions
+
+//In this exercise we will be creating a webpage with a black background as the solar system and we will fill the solar system with planets and their moons.
+//We will provide the HTML page.
+//You only have to work with a JS file.
+
+// 1. Create an array which value is the planets of the solar system.
+
+let planets = [
+  `Earth`,
+  `Mars`,
+  `Uranus`,
+  `Jupiter`,
+  `Neptune`,
+  `Mercury`,
+  `Saturn`,
+  `Venus`,
+];
+
+// 2. For each planet in the array, create a <div> using createElement. This div should have a class named "planet".
+
+// 3. Each planet should have a different background color. (Hint: you could add a new class to each planet - each class containing a different background-color).
+
+let arrayOfColors = [
+  "blue",
+  "red",
+  "lightblue",
+  "orange",
+  "darkblue",
+  "gray",
+  "yellow",
+  "lightgreen",
+];
+
+// 4. Finally append each div to the <section> in the HTML (presented below).
+
+let listPlanetsSection = document.querySelector(".listPlanets");
+
+planets.forEach((planet, index) => {
+  let planetDiv = document.createElement("div");
+
+  planetDiv.className = "planet";
+
+  planetDiv.textContent = planet;
+
+  planetDiv.style.backgroundColor = arrayOfColors[index];
+
+  listPlanetsSection.appendChild(planetDiv);
+});
+
+// 5. Bonus: Do the same process to create the moons.
+
+// Be careful, each planet has a certain amount of moons. How should you display them?
+
+// Should you still use an array for the planets ? Or an array of objects ?
+
+// Couldnt find a solution for the bonus, check it out in class!!
