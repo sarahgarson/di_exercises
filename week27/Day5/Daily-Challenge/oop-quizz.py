076901fbd51392d6f901b93745b2e3a94b3ed1d9
Sarah Garson <sarah.garson.santos@gmail.com>
2024-11-24 19:34:59 +0200

    rock-paper-scissors-fixes

---
+++ week27/Day5/Daily-Challenge/oop-quizz.py
@@ -0,0 +1,28 @@
+# Instructions
+# Part 1 : Quizz :
+# Answer the following questions
+
+# What is a class?
+# a class is a 
+
+
+# What is an instance?
+
+
+# What is encapsulation?
+
+
+# What is abstraction?
+
+
+# What is inheritance?
+
+
+# What is multiple inheritance?
+
+
+# What is polymorphism?
+
+
+# What is method resolution order or MRO?
+
--- week27/Day5/Mini-Projects/Rock-Paper-Scissors/game.py
+++ week27/Day5/Mini-Projects/Rock-Paper-Scissors/game.py
@@ -35,8 +35,8 @@ class Game:
          user_item = input("Please select an item (rock/paper/scissors): ")
          if user_item in ["rock", "paper", "scissors"]:
             return user_item
-         else:
-            print("Invalid input. Please try again.")
+         
+         print("Invalid input. Please try again.")
 
    def get_computer_item(self):
       return random.choice(["rock", "paper", "scissors"])
@@ -44,10 +44,11 @@ class Game:
    def get_game_result(self, user_item, computer_item):
       if user_item == computer_item:
          return "draw"
-      elif (user_item == "rock" and computer_item == "scissors") or (user_item == "paper" and computer_item == "rock") or (user_item == "scissors" and computer_item == "paper"):
+      
+      if (user_item == "rock" and computer_item == "scissors") or (user_item == "paper" and computer_item == "rock") or (user_item == "scissors" and computer_item == "paper"):
          return "win"
-      else:
-         return "loss"
+      
+      return "loss"
       
    def play(self):
       user_item = self.get_user_item()
